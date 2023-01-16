b0405421994b87856b746608717b3325d15ec362
MlotPL <79142696+MlotPL@users.noreply.github.com>
2023-01-16 21:04:55 +0100

    Create font.css

---
+++ files/assets/css/font.css
@@ -0,0 +1,7 @@
+@font-face {
+font-family: "CustomFont";
+src: url("https://yoursite.com/css/fonts/CustomFont.eot");
+src: url("https://yoursite.com/css/fonts/CustomFont.woff") format("woff"),
+url("https://yoursite.com/css/fonts/CustomFont.otf") format("opentype"),
+url("https://yoursite.com/css/fonts/CustomFont.svg#filename") format("svg");
+}
