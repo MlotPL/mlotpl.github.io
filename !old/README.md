b728001c00470ff97846bccdcdbe9567571a891c
MlotPL <79142696+MlotPL@users.noreply.github.com>
2025-02-24 07:46:54 +0100

    Delete !old directory

--- !old/README.md
+++
@@ -1,6 +0,0 @@
-
-<a>ENG:
-old versions of website. Discontinued for now.</a> <br>
-<a>PL:
-stare wersje strony
-zrobie to lepiej, poprawie kod, zrobie go krótszym itd. Kiedyś...</a>
--- !old/files/assets/css/font.css
+++
@@ -1,7 +0,0 @@
-@font-face {
-font-family: "CustomFont";
-src: url("https://yoursite.com/css/fonts/CustomFont.eot");
-src: url("https://yoursite.com/css/fonts/CustomFont.woff") format("woff"),
-url("https://yoursite.com/css/fonts/CustomFont.otf") format("opentype"),
-url("https://yoursite.com/css/fonts/CustomFont.svg#filename") format("svg");
-}
